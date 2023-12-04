7f10ab2b8ad8ea834e904c32de34635221188f8e
stpafk <stephan.weigert@hotmail.com>
2023-12-04 10:30:06 -0300

    set route: get and post for register

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const controller = require('../controllers/indexController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  return res.status(200);
 });
+
+router.get('/register', controller.register_get);
+router.post('/register', controller.register_post)
 
 module.exports = router;
