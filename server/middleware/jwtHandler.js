9613c5d2fed159d4847c6addc493b2cc3325e6f4
stpafk <stephan.weigert@hotmail.com>
2023-12-04 11:57:30 -0300

    add jwt validate token

--- server/middleware/jwtHandler.js
+++ server/middleware/jwtHandler.js
@@ -1,7 +1,33 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
+var secret = process.env.SECRET;
+
 exports.get_token = function(id) {
-    const secret = process.env.SECRET;
     return jwt.sign({id: id}, secret);
+};
+
+exports.header_token = function(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+
+    if (typeof bearerHeader !== 'undefined') {
+        const token = bearerHeader.split(' ')[1];
+        req.token = token;
+
+        next();
+    }
+
+    return res.status(403);
+};
+
+exports.validate_token = async (req, res, next) => {
+    jwt.verify(req.token, secret, async (err) => {
+        if (err) {
+            return res.status(403).json({
+                "alert": "Access Denied."
+            });
+        }
+
+        next()
+    });
 };
