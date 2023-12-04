9095bf1c6bbfca88a39cfd07a0319ddf5fb118d6
stpafk <stephan.weigert@hotmail.com>
2023-12-04 09:43:27 -0300

    add mongo

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 
@@ -14,6 +15,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
+
+//connect to DATABASE
+
+const MONGO = process.env.MONGO;
+
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect(MONGO);
+};
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -28,7 +38,6 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
