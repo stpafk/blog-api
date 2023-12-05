fe1236f870b86cbfae643e78e1a7ad257e85ecef
stpafk <stephan.weigert@hotmail.com>
2023-12-05 15:42:33 -0300

    add route for message

--- server/routes/post.js
+++ server/routes/post.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const postController = require('../controllers/postController');
+const messageController = require('../controllers/messagesController');
 const jwtHandler = require('../middleware/jwtHandler');
 
 router.get('/:id', postController.get_post)
@@ -41,5 +42,17 @@ router.delete('/:id/delete',
     jwtHandler.validate_token,
     postController.delete_delete,
 );
+
+router.post('/:id/message', 
+    jwtHandler.header_token,
+    jwtHandler.validate_token,
+    messageController.post_message
+);
+
+router.delete('/:id/message/:messageId',
+    jwtHandler.header_token,
+    jwtHandler.get_token,
+    messageController.delete_message,
+)
 
 module.exports = router;
