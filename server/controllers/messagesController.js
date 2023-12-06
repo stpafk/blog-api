44bd61615e473687b3b156318508ac12b1f45f8d
stpafk <stephan.weigert@hotmail.com>
2023-12-06 07:55:10 -0300

    feat: POST message

---
+++ server/controllers/messagesController.js
@@ -0,0 +1,65 @@
+const Post = require('../models/posts');
+const Message = require('../models/messages');
+const User = require('../models/users');
+require('dotenv').config
+const {validationResult, body} = require('express-validator');
+const { isValidObjectId } = require('mongoose');
+
+exports.post_message = [
+
+    body("userId").custom(value =>  {
+            const validate = isValidObjectId(value);
+            if (!validate) {
+                throw new Error("Invalid ID");
+            }
+        }
+    )
+    .escape(),
+    body("content").notEmpty().escape(),
+
+    async function(req, res, next) {
+
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(403).json({
+                errors
+            })
+        }
+
+        const postId = req.params.id;
+        const [post, user] = await Promise.all([
+            Post.findById(postId).exec().catch(err => console.log(err)),
+            User.findById(req.body.userId).exec(),
+        ]);
+
+        if (post.length === 0) {
+            return res.sendStatus(404);
+        };
+
+        if (user.length === 0) {
+            return res.sendStatus(403);
+        }
+
+        const message = new Message({
+            post: post._id,
+            username: user._id,
+            content: req.body.content,
+            time_stamp: new Date(),
+        });
+
+        await message.save();
+        res.status(201).json({
+            "user": {
+                user
+            },
+            "message": {
+                message,
+            }
+        });
+    }
+]
+
+exports.delete_message = async function(req, res, next) {
+
+}
