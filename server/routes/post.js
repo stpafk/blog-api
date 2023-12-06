b7e3c0e971351369c97e1d2e04bf9fb4027a9c71
stpafk <stephan.weigert@hotmail.com>
2023-12-06 10:33:02 -0300

    feat: validateId middleware

--- server/routes/post.js
+++ server/routes/post.js
@@ -4,8 +4,12 @@ var router = express.Router();
 const postController = require('../controllers/postController');
 const messageController = require('../controllers/messagesController');
 const jwtHandler = require('../middleware/jwtHandler');
+const validateId = require('../middleware/validateId');
 
-router.get('/:id', postController.get_post)
+router.get('/:id', 
+    validateId,
+    postController.get_post 
+);
 
 router.get('/create', 
     jwtHandler.validate_token, 
@@ -18,27 +22,32 @@ router.post('/create',
 );
 
 router.get('/:id/update', 
+    validateId,
     jwtHandler.validate_token,
     postController.get_update,
 );
 
 router.put('/:id/update',
     jwtHandler.validate_token,
+    validateId,
     postController.put_update,
 );
 
 router.get('/:id/delete',
     jwtHandler.validate_token,
+    validateId,
     postController.get_delete,
 );
 
 router.delete('/:id/delete',
     jwtHandler.validate_token,
+    validateId,
     postController.delete_delete,
 );
 
 router.post('/:id/message', 
     jwtHandler.validate_token,
+    validateId,
     messageController.post_message
 );
 
