549ae2c5632601c4b8faa6d245900eed62367a01
stpafk <stephan.weigert@hotmail.com>
2023-12-07 11:18:28 -0300

    fixed: register and login form dispalying title

--- client-blog/src/components/Form/LoginForm.jsx
+++ client-blog/src/components/Form/LoginForm.jsx
@@ -1,8 +1,8 @@
 import Footer from "../UI/Footer";
 import Header from "../UI/Header/Header";
 import * as Form from "./Style";
-// Idea: maybe have a parent element that handles login/register
-export default function RegisterForm() {
+
+export default function LoginForm() {
 
     return (
         <>
@@ -11,24 +11,12 @@ export default function RegisterForm() {
                     <Form.StyledLabel>Username</Form.StyledLabel>
                     <Form.StyledInput type="text" id="username" />
                 </Form.StyledFormContent>
-
-                <Form.StyledFormContent id="email">
-                    <Form.StyledLabel>Email</Form.StyledLabel>
-                    <Form.StyledInput type="text" id="email" />
-                </Form.StyledFormContent>                    
-
                 <Form.StyledFormContent id="password">
                     <Form.StyledLabel>Password</Form.StyledLabel>
                     <Form.StyledInput type="password" id="password" value="" />
                 </Form.StyledFormContent>
-
-                <Form.StyledFormContent id="password_confirm">
-                    <Form.StyledLabel>Confirm Password</Form.StyledLabel>
-                    <Form.StyledInput type="password" id="password_confirm" value="" />
-                </Form.StyledFormContent>
-
                 <Form.StyledButton type="submit">Submit</Form.StyledButton>
-            </Form.StyledForm>       
+            </Form.StyledForm>          
         </>
     )
 
