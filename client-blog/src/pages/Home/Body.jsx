399a49f29cedf17e68230d41d2a3aa7cf427f1f5
stpafk <stephan.weigert@hotmail.com>
2023-12-06 21:29:56 -0300

    add fetch request to home

---
+++ client-blog/src/pages/Home/Body.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+export default function Body() {
+
+    const [loading, setLoading] = useState(true)
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:3000/", {
+            method: "GET",
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                "Content-Type": "application/json"
+            }
+        })
+        .then((request) => {
+            return request.json()
+        })
+        .then((data) => setPosts(data))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false))
+    }, [])
+
+    if (loading) {
+        return (
+            <p>Fetching resource...</p>
+        )
+    }
+
+    return(
+        <ul>
+            {posts["posts"].map(post => {
+                return <li key={post._id}>
+                    <p>{post.title}</p>
+                    <p>{post.content}</p>
+                </li>
+            })}
+        </ul>
+    )
+};
