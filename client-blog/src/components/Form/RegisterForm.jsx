6737436541ebf176334ad7b901b362a9c8c6ec37
stpafk <stephan.weigert@hotmail.com>
2023-12-08 09:11:38 -0300

    feat: register and register form

--- client-blog/src/components/Form/RegisterForm.jsx
+++ client-blog/src/components/Form/RegisterForm.jsx
@@ -1,30 +1,56 @@
+import { useState } from "react";
 import Footer from "../UI/Footer";
 import Header from "../UI/Header/Header";
 import * as Form from "./Style";
 // Idea: maybe have a parent element that handles login/register
-export default function RegisterForm() {
+export default function RegisterForm(props) {
+
+    const [value, setValues] = useState({
+        "username": "",
+        "email": "",
+        "password": "",
+        "password_confirm": "",
+    });
+    
+    const handleSubmit = props.handleSubmit;
+    const handleChange = (e) => {
+        setValues({
+            ...value,
+            [e.target.id]: e.target.value
+        });
+    };
 
     return (
         <>
-            <Form.StyledForm method="POST" action="">
+            <Form.StyledForm onSubmit={handleSubmit}>
                 <Form.StyledFormContent id="username">
                     <Form.StyledLabel>Username</Form.StyledLabel>
-                    <Form.StyledInput type="text" id="username" />
+                    <Form.StyledInput type="text" 
+                    id="username" 
+                    value={value.username}
+                    onChange={handleChange}/>
                 </Form.StyledFormContent>
 
                 <Form.StyledFormContent id="email">
                     <Form.StyledLabel>Email</Form.StyledLabel>
-                    <Form.StyledInput type="text" id="email" />
+                    <Form.StyledInput type="text" 
+                    id="email" 
+                    value={value.email}
+                    onChange={handleChange}/>
                 </Form.StyledFormContent>                    
 
                 <Form.StyledFormContent id="password">
                     <Form.StyledLabel>Password</Form.StyledLabel>
-                    <Form.StyledInput type="password" id="password" value="" />
+                    <Form.StyledInput type="password" id="password"
+                    value={value.password}
+                    onChange={handleChange} />
                 </Form.StyledFormContent>
 
                 <Form.StyledFormContent id="password_confirm">
                     <Form.StyledLabel>Confirm Password</Form.StyledLabel>
-                    <Form.StyledInput type="password" id="password_confirm" value="" />
+                    <Form.StyledInput type="password" id="password_confirm" 
+                    value={value.password_confirm}
+                    onChange={handleChange}/>
                 </Form.StyledFormContent>
 
                 <Form.StyledButton type="submit">Submit</Form.StyledButton>
--- client-blog/src/pages/Register/Register.jsx
+++ client-blog/src/pages/Register/Register.jsx
@@ -1,15 +1,51 @@
 import RegisterForm from "../../components/Form/RegisterForm";
 import Footer from "../../components/UI/Footer";
 import Header from "../../components/UI/Header/Header";
+import useIsLogged from "../../hooks/useIsLogged";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
 
+    const isLogged = useIsLogged();
+    const nav = useNavigate();
+    if (isLogged) {
+        nav('/');
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch("http://localhost:3000/register", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "username": e.target.elements.username.value,
+                "email": e.target.elements.email.value,
+                "password": e.target.elements.password.value,   
+                "password_confirm": e.target.elements.password_confirm.value,
+            }),
+        })
+        .then(res => {
+            if (res.status >= 400) {
+                alert("error")
+                //need to implement error handling
+                return;
+            };
+            
+            res.json();
+        })
+        .then(nav("/"))
+        .catch(err => console.log(err))
+    }
+    
     return (
         <>
         <Header />
         <main>
             <h1>Register</h1>
-            <RegisterForm />
+            <RegisterForm handleSubmit={handleSubmit}/>
         </main>
         <Footer />
         </>
