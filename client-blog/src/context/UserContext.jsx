3d190a16c76dff22c21f2fa3d39711942d4588fb
stpafk <stephan.weigert@hotmail.com>
2023-12-12 11:21:50 -0300

    refactor: userContext

--- client-blog/src/context/UserContext.jsx
+++ client-blog/src/context/UserContext.jsx
@@ -1,29 +1,23 @@
 import React, { useContext, useState } from "react";
 
 const UserContext = React.createContext();
-const UpdateUser = React.createContext();
 
 export function useUserContext() {
     return useContext(UserContext);
-};
-
-export function useUpdateUserContext() {
-    return useContext(UpdateUser);
 };
 
 export default function UserProvider({children}) {
     const [user, setUser] = useState({});
 
     function handleUserData(data) {
+        console.log(data);
         setUser(data);
     };
 
     return (
         <>
-        <UserContext.Provider value={user}>
-            <UpdateUser.Provider value={handleUserData}>
-                {children}
-            </UpdateUser.Provider>
+        <UserContext.Provider value={[user, handleUserData]}>
+            {children}
         </UserContext.Provider>
         </>
     )
