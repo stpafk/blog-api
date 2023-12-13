43a661ce8964b9329f420c3c0e5b8a81f06bc3d0
stpafk <stephan.weigert@hotmail.com>
2023-12-13 16:27:42 -0300

    feat: submit form comment to db

--- client-blog/src/pages/Post/Post.jsx
+++ client-blog/src/pages/Post/Post.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 import Header from "../../components//UI//Header/Header";
 import Footer from "..//../components/UI/Footer"
 import { useLoaderData, Outlet } from "react-router-dom";
+import { useUserContext } from "../../context/UserContext";
+import { useIsLogged } from "../../context/LoggedContext";
 
 export default function Post() {
 
     const user = useLoaderData();
+    const [, updateUser] = useUserContext();
+    const [, update] = useIsLogged();
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        if (user) {
+            updateUser(user);
+            update(true);
+            setLoading(false);
+        }
+
+        setLoading(false)
+    })
+
+    if (loading) {
+        return(<></>)
+    }
 
     return (
         <>
--- client-blog/src/pages/Post/PostId.jsx
+++ client-blog/src/pages/Post/PostId.jsx
@@ -9,7 +9,7 @@ import CommentForm from "../../components/Form/CommentForm";
 export default function PostId() {
 
     const [loading, setLoading] = useState(true);
-    const [post, setPost] = useState({});
+    const [post, setPost] = useState([]);
     const [error, setError] = useState(false);
     const { postId } = useParams();
 
@@ -22,20 +22,54 @@ export default function PostId() {
             setLoading(false)
         })
         .catch(err => {
-            console.log(err) 
-            setError(true)
+            console.log(err);
+            setError(true);
+            setLoading(false); 
         })
 
-    }, [])
+    }, [postId])
+
+    function submitMessage(e) {
+        e.preventDefault();
+        fetch(`http://localhost:3000/post/${postId}/message`, {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "content": e.target.elements.content.value,
+            })})
+            .then(res => {
+
+                if (res.status !== 201) {
+                    throw new Error("Not logged to send message.")
+                }
+
+                return res.json();
+            })
+            .then(data => {
+                let message = data.message.message;
+                setPost(prev => ({
+                    ...prev,
+                    messages: {
+                        messages: [message, ...prev.messages.messages]
+                    }
+                }))
+            })
+            .catch(err => console.log(err))
+
+    }
     
     if (loading) return <main>Fetching data....</main>
     if (error) return <ErrorPage />
+
 
     return(
         <main>
             <Article post={post}/>
             <div>
-                <CommentForm />
+                <CommentForm submitMessage={submitMessage}/>
                 <Comments messages={post.messages.messages}/>
             </div>
         </main>
