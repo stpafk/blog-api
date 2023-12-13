fac9b904966f0f06445ac38a3554d981ed4c1a63
stpafk <stephan.weigert@hotmail.com>
2023-12-13 16:27:13 -0300

    feat: comment form

--- client-blog/src/components/Form/CommentForm.jsx
+++ client-blog/src/components/Form/CommentForm.jsx
@@ -1,4 +1,19 @@
+import { Link } from "react-router-dom";
+import { useUserContext } from "../../context/UserContext";
 
-export default function CommentForm() {
-    return(<></>)
+export default function CommentForm({submitMessage}) {
+    const [user, ] = useUserContext();
+
+    return (
+        user.user ? 
+    <form onSubmit={submitMessage}>
+        <label htmlFor="content">New Message</label>
+        <textarea name="content" id="content" cols="30" rows="2"></textarea>
+        <button type="submit">Submit</button>
+    </form> : 
+    <div>   
+        <p>Log In to send message</p>
+        <Link to="/user/login">Log In</Link>
+    </div>
+    )
 }
