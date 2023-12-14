bd05ed03c1182cf46388d79db5c00b97c948a7e5
stpafk <stephan.weigert@hotmail.com>
2023-12-14 11:45:08 -0300

    feat: handledeletemessage on comment

--- client-blog/src/components/UI/Post/Comments.jsx
+++ client-blog/src/components/UI/Post/Comments.jsx
@@ -1,14 +1,32 @@
 import { useState } from "react";
 import { useUserContext } from "../../../context/UserContext"
+import { useParams } from "react-router-dom";
+import { handleDeleteMessage } from "../../../utils/handleDeleteMessage";
 
 export default function Comments({messages}) {
 
+    const { postId } = useParams();
     const [user, ] = useUserContext();
     const [messageHandler, setMessageHandler] = useState(messages.map(message => ({
         ...message,
         deleting: false,
     })));
 
+    function submitDelete(messageId) {
+
+        setMessageHandler(prev => prev.filter(message =>
+            message._id !== messageId))
+        
+        handleDeleteMessage(postId, messageId)
+        .catch(err => console.log(err))
+    }
+
+    function handleDelete(id) {
+        setMessageHandler(messageHandler.map(message => message._id === id ? {
+            ...message, deleting: !message.deleting
+        } : message))
+    }
+    // future change: create two components to handle both delete and display data
     return(
         <>
         <section>
@@ -16,7 +34,9 @@ export default function Comments({messages}) {
             {messageHandler.map((message) => {
                 return <li key={message._id} id={message._id}>
                     {message.deleting ? <div>
-                        <p>Do you want to delete?</p>
+                        <p>Do you want to delete this message?</p>
+                        <button onClick={() => submitDelete(message._id)}>Delete</button>
+                        <button onClick={() => handleDelete(message._id)}>Cancel</button>
                     </div> : <>
                             <p>{message.username.username}</p>
                             <p>{message.time_stamp}</p>
