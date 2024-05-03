e3ee8175fb33370762487f4b5424253b44cf9372
stpafk <stephan.weigert@hotmail.com>
2024-05-03 13:57:20 -0300

    fixed: links

--- client-blog/src/pages/Router.jsx
+++ client-blog/src/pages/Router.jsx
@@ -9,6 +9,7 @@ import Index from "./User/Index";
 import { fetchUser } from "../utils/handleFetchUser";
 import Post from "./Post/Post";
 import PostId from "./Post/PostId";
+import Admin from "./Admin/Admin";
 
 
 export default function Router() {
@@ -39,6 +40,18 @@ export default function Router() {
             loader: async () => { return await fetchUser()},
             children: [
                 {path: ":postId", element: <PostId />}
+            ]
+        },
+        {
+            path: "/admin",
+            element: <Admin />,
+            errorElement: <ErrorPage />,
+            children: [
+                // {path: "/create", element: null},
+                // {path: "/posts", element: null},
+                // {path: "/post/:id/update", element: null},
+                // {path: "/post/:id/delete", element: null},
+                // {path: "/messages", element: null},
             ]
         }
     ])
